52eb0326f25c83b5896df1630d6adacf4b43107b
sergiomgaspar <sergiomgaspar.dev@gmail.com>
2017-01-12 19:03:13 +0100

    small update, changed listen port

--- timestamp.js
+++ timestamp.js
@@ -3,7 +3,9 @@
 */
 var express = require('express');
 var app = express();
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/home', function(req, res) {
-    res.end('Hello World!')
+    res.end('Hello World! \nNode running on port: '+ app.get('port'));
 });
-app.listen(process.argv[2]);
+//app.listen(process.argv[2]);
