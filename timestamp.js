c01ab35616f6d68a8f3437f3464a83d2009cf348
sergiomgaspar <sergiomgaspar.dev@gmail.com>
2017-01-16 20:49:04 +0100

    initial timestamp express app

--- timestamp.js
+++ timestamp.js
@@ -2,10 +2,25 @@
 	Hello world
 */
 var express = require('express');
+var fs = require('fs');
+var http = require('http');
 var app = express();
-app.set('port', (process.env.PORT || 5000));
+var port = process.env.PORT || 3000;
+
+var html = fs.readFileSync('index.html');
 
 app.get('/home', function(req, res) {
     res.end('Hello World! \nNode running on port: '+ app.get('port'));
 });
-//app.listen(process.argv[2]);
+
+/* / path, return default html */
+app.get('/', function(req, res) {
+	res.writeHead(200);
+	res.write(html);
+	res.end();
+});
+
+// Listen on port 3000, IP defaults to 127.0.0.1
+app.listen(port);
+
+console.log('Server running at http://127.0.0.1:' + port + '/');
