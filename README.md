280c7fb8bc2df8b2aeecd527f510fa5c641bd3bd
sergiomgaspar <sergiomgaspar.dev@gmail.com>
2017-01-17 18:15:02 +0100

    final stable version

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# smg-timestamp
-Rep for Timestamp node exercise (FCC)
+# Timestamp API
+
+This api was created for the FreeCodeCamp exercice "Timestamp Microservice"
+
+* [FreeCodeCamp] - Link to the exercice
+* [Heroku] - Online deployed version
+
+### API description
+
+The Timestamp API must return the unix value date as well as the natural date of an argument passed in the URL while implementing the use-cases below.
+
+  - **Use-case 1:** User can pass a string as a parameter, and it will check to see whether that string contains either a unix timestamp or a natural language date *(**example**: January 1, 2016)*
+  - **Use-case 2:**  If it does, it returns both the Unix timestamp and the natural language form of that date
+  - **Use-case 3:**  If it does not contain a date or Unix timestamp, it returns **null** for those properties
+
+## Example (usage)
+Use the URL below to get the date in the desired format
+```sh
+https://smg-timestamp.herokuapp.com/August%2028,%201981
+https://smg-timestamp.herokuapp.com/367857601
+```
+## Example (result)
+Below is shown the result returned from the URL above.
+```sh
+{ "unix": 367857601, "natural": "August 28, 1981" }
+```
+## Technologies used
+This is a very small example of an API created in NodeJS using the Express Framework.
+
+## License
+
+[![CC0](http://i.creativecommons.org/p/zero/1.0/88x31.png)](http://creativecommons.org/publicdomain/zero/1.0/)
+
+To the extent possible under law, the author has waived all copyright and related or neighboring rights to this work.
--- timestamp.js
+++ timestamp.js
@@ -1,26 +1,115 @@
-/*
-	Hello world
+/* 
+	Timestamp Microservice for FreeCodeCamp
+	by: Sergio Gaspar
+	date: 2017/01/17
+	
+	Create miscroservice with the below user stories:
+	
+	US1: I can pass a string as a parameter, and it will check to see whether that string contains either a unix timestamp or a natural language date (example: January 1, 2016).
+	US2: If it does, it returns both the Unix timestamp and the natural language form of that date.
+	US3: If it does not contain a date or Unix timestamp, it returns null for those properties.User
 */
 var express = require('express');
 var fs = require('fs');
-var http = require('http');
+var url = require('url');
 var app = express();
 var port = process.env.PORT || 3000;
-
+var isDebug = false;
 var html = fs.readFileSync('index.html');
 
-app.get('/home', function(req, res) {
-    res.end('Hello World! \nNode running on port: '+ app.get('port'));
+// Return null representation of the object
+function getNullResponse(){
+	return JSON.stringify({"unix": null,"natural": null});
+};
+
+// Return expected response from parsed date input
+function getResponse(parseDt){
+	var monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+	var timestamp;
+	
+	if (isNaN(parseDt)){
+		// String date format in URL
+		timestamp = Date.parse(parseDt);
+	
+	} else {
+		// Numeric inserted in URL (note: input in seconds, timestamp in miliseconds)
+		timestamp = parseDt * 1000;
+	}
+	
+	var inDate=new Date();
+	inDate.setTime(timestamp);
+	
+	// Date info for DEBUG only
+	Log('D','timestamp: '+timestamp);
+	Log('D',"inDate.getTime: "+inDate.getTime());
+	Log('D',"inDate.getFullYear(): "+inDate.getFullYear());
+	Log('D',"inDate.getMonth(): "+inDate.getMonth());
+	Log('D',"inDate.getUTCDate(): "+inDate.getUTCDate());
+
+	// Convert from miliseconds to seconds
+	var secDt = inDate.getTime() / 1000;
+	
+	return JSON.stringify({
+		"unix": secDt,
+		// example: January 1, 2016
+		"natural": monthNames[inDate.getMonth()]+' '+inDate.getUTCDate()+', '+inDate.getFullYear()
+	});
+};
+
+// Simple Log function to print to console
+function Log(level, logStr){
+	if (isDebug && level === 'D') console.log("DEBUG: "+logStr);
+		else if (level === 'I') console.log("INFO: "+logStr);
+			else if (level === 'E') console.log("ERROR: "+logStr);
+				else console.log(level+": "+logStr);
+};
+
+// START: String replace all aux functions
+function escapeRegExp(str) {
+    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
+
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+// END: String replace all aux functions
+
+// Express app handles all GET regardless of URL (no routing)
+app.get('/*', function(req, res) {
+	var parts = url.parse(req.url, true);
+	var path = parts.path;
+		
+	if (path === '/') {
+		// return index.html if no timestamp requested
+		res.writeHead(200);
+		res.write(html);
+		res.end();
+	} else if (path === '/favicon.ico') {
+		// Handle the site icon request... return nothing
+		res.writeHead(200);
+		res.end();
+		} else {
+			path = path.slice(1);				// Remove '/'
+			path = replaceAll(path,'%20',' ');	// Handles spaces in the URL
+			
+			Log('I','URL: '+path);
+			res.writeHead(200);
+			
+			if ((isNaN(path)) && (isNaN(Date.parse(path)))) {
+				Log("D","Not a proper date inserted, returning null.");
+				res.write(getNullResponse());
+			} else {
+				Log("D","Date inserted, determining output");
+				res.write(getResponse(path));
+			}
+			res.end();
+	}
+	
 });
 
-/* / path, return default html */
-app.get('/', function(req, res) {
-	res.writeHead(200);
-	res.write(html);
-	res.end();
-});
-
-// Listen on port 3000, IP defaults to 127.0.0.1
+// Listen on port 3000 by default, IP defaults to 127.0.0.1
 app.listen(port);
 
-console.log('Server running at http://127.0.0.1:' + port + '/');
+// Logs port it is running on
+Log('I','Server running at http://127.0.0.1:' + port + '/');
+// URL to execute:  http://npm.sergiomgaspar.c9users.io/
